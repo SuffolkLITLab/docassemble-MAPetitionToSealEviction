77e70ecbceeecde83a47c646f57a5dddae00d39b
Sam Glover <sam.glover@suffolk.edu>
2025-04-22 12:06:30 -0400

    Non-substantive edits to the Petition_to_Seal_Eviction screen

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Petition to Seal Eviction
 
 ## Author
 
+Rami
 Taline
-
--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.MAPetitionToSealEviction',
       version='1.4.1',
       description=('Petition to Seal Eviction'),
-      long_description='# docassemble.PetitionToSealEviction\r\n\r\nPetition to Seal Eviction\r\n\r\n## Author\r\n\r\nTaline\r\n\r\n',
+      long_description='# docassemble.PetitionToSealEviction\r\n\r\nPetition to Seal Eviction\r\n\r\n## Author\r\n\r\nRami\r\nTaline',
       long_description_content_type='text/markdown',
       author='Taline',
       author_email='rami.nabhan@su.suffolk.edu',
